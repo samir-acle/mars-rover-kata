4425e91fd596321f8e2d1eb74bdad1646b8c6001
Samir Mehta <sammehta88@gmail.com>
2016-09-07 21:37:51 -0400

    add initial test outlines

--- test/rover-spec.js
+++ test/rover-spec.js
@@ -1,2 +1,53 @@
 var chai = require('chai');
 var assert = chai.assert;
+var rover - require('rover');
+
+describe('mars rover', function() {
+  describe('initialization of rover', function() {
+    it('should set the initial location', function() {
+
+    });
+
+    it('should set the direction the rover is facing', function() {
+
+    });
+  });
+
+  describe('rover movement', function() {
+    describe('command given is f', function() {
+      it('should increase x location by 1 when facing E', function() {
+
+      });
+
+      it('should decrease x location by 1 when facing W', function() {
+
+      });
+
+      it('should increase y location by 1 when facing S', function() {
+
+      });
+
+      it('should decrease y location by 1 when facing N', function() {
+
+      })
+    });
+
+    describe('command given is b', function() {
+      it('should decrease x location by 1 when facing E', function() {
+
+      });
+
+      it('should increase x location by 1 when facing W', function() {
+
+      });
+
+      it('should decrease y location by 1 when facing S', function() {
+
+      });
+
+      it('should increase y location by 1 when facing N', function() {
+
+      })
+    });
+  });
+});
