3e4525d6c55621a3d46161e14cfec90c98107e32
Samir Mehta <sammehta88@gmail.com>
2016-09-07 23:44:49 -0400

    implement turn functionality making tests pass

--- drive-system.js
+++ drive-system.js
@@ -5,8 +5,26 @@ const NAVIGATION = {
   S: {direc: 1, axis: 'y'}
 };
 
-module.exports.move = function(direction) {
+const COMPASS = ['N', 'E', 'S', 'W'];
+
+function moveForOrBack(direction) {
   var fOrB = direction === 'f' ? 1 : -1;
   var navInfo = NAVIGATION[this.facing];
   this.location[navInfo.axis] += navInfo.direc * fOrB;
+}
+
+
+function turnLeftOrRight(direction) {
+  var lOrR = direction === 'r' ? 1 : -1;
+  var newPos = COMPASS.indexOf(this.facing) + lOrR;
+  newPos = newPos > 3 ? 0 : (newPos < 0 ? 3 : newPos);
+  this.facing = COMPASS[newPos];
+}
+
+module.exports.move = function(direction) {
+  if (direction === 'f' || direction === 'b') {
+    moveForOrBack.call(this, direction);
+  } else if (direction === 'l' || direction === 'r') {
+    turnLeftOrRight.call(this, direction);
+  }
 };
